66f302954ea271851c747b5fd54281656f01b9a5
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2024-07-05 06:45:21 +0300

    تم إنشاء مشروع جديد بالوضع الأصلي

---
+++ src/zakat_tracker/app.py
@@ -0,0 +1,26 @@
+"""
+Personal Accounting Software using Zakat way for all transactions from the beginning to the end
+"""
+
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class ZakatTracker(toga.App):
+    def startup(self):
+        """Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return ZakatTracker()
